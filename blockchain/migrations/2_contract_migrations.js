5b32e2e532b7369acbebf1521cb4d14744c67a38
vvdhout <valentijnvdh@gmail.com>
2019-02-26 16:44:31 +0100

    Adding contract migrations.

---
+++ blockchain/migrations/2_contract_migrations.js
@@ -0,0 +1,15 @@
+// migrating the appropriate contracts
+var HarvesterRole = artifacts.require("./HarvesterRole.sol");
+var ManufacturerRole = artifacts.require("./ManufacturerRole.sol");
+var DistributorRole = artifacts.require("./DistributorRole.sol");
+var RetailerRole = artifacts.require("./RetailerRole.sol");
+var ConsumerRole = artifacts.require("./ConsumerRole.sol");
+var SupplyChain = artifacts.require("./SupplyChain.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(HarvesterRole);
+  deployer.deploy(DistributorRole);
+  deployer.deploy(RetailerRole);
+  deployer.deploy(ConsumerRole);
+  deployer.deploy(SupplyChain);
+};
